a692d87b56826a9ad3f63f5cf59c8aac9d9ef16d
unibreakfast <unibreakfast1@gmail.com>
2019-12-21 11:57:22 +0200

    test function added to tests

--- lapland.test.js
+++ lapland.test.js
@@ -1,22 +1,24 @@
 
 const
 c = console.log,
+test = title => [failMsg => c("TEST: "+title) || c("FAIL: "+failMsg),
+                   okMsg => c("TEST: "+title) || c("OK: "+okMsg)]
 
 moduleSpec = async ()=> {
-  c("lapland module supposed to be exporting an object with one method - an async function .wish(details)")
+  const [ fail, ok ] = test("lapland module supposed to be exporting an object with one method - an async function .wish(details)")
 
   const lapland = require('./lapland')
   if (typeof lapland != 'object')
-    c("lapland.js doesn't export an object")
+    fail("lapland.js doesn't export an object")
   else if (Object.keys(lapland).length != 1)
-    c("lapland object expected to have exactly one property")
+    fail("lapland object expected to have exactly one property")
   else if (!lapland.wish || typeof lapland.wish != 'function')
-    c("there's no .wish(details) method")
+    fail("there's no .wish(details) method")
   else if (lapland.wish.length != 1)
-    c("the .wish() method supposed to expect one argument")
+    fail("the .wish() method supposed to expect one argument")
   else if (!(lapland.wish({}) instanceof Promise))
-    c(".wish(details) is not an async function")
-  else c("and it does!")
+    fail(".wish(details) is not an async function")
+  else ok("and it does!")
 }
 
 moduleSpec()
