40783593898a1b7161050a077034d51efac586a8
unibreakfast <unibreakfast1@gmail.com>
2019-12-21 17:27:57 +0200

    npm unsummon command to remove modules

--- summon.test.js
+++ summon.test.js
@@ -2,7 +2,8 @@
 const
 c = console.log,
 fsp = require('fs').promises,
-exec = require('child_process').exec,
+exec = command => new Promise(resolve =>
+  require('child_process').exec(command, resolve)),
 test = (title, finish, err) => [
   failMsg => !err && c("TEST: "+title) || c("FAIL: "+failMsg) || (err=1),
   ()=> !err && (c("TEST: "+title) || c("OK: "+finish))
@@ -16,18 +17,35 @@ allModules = async ()=> {
 
   await Promise.all(modules.map(m => fsp.rmdir(m, {recursive: true})))
 
-  exec("npm run summon", async ()=>{
-    for (const m of modules.slice()) {
-      try {
-        if ((await fsp.stat(m)).isDirectory())
-          modules = modules.filter(mod => m!=mod)
-      } catch {}
-    }
-    modules.length?
-      fail("but it doesn't clone these modules: "+modules.join(', ')) : ok()
-  })
+  await exec("npm run summon")
+  for (const m of modules.slice()) {
+    try {
+      if ((await fsp.stat(m)).isDirectory())
+        modules = modules.filter(mod => m!=mod)
+    } catch {}
+  }
+  modules.length?
+    fail("but it doesn't clone these modules: "+modules.join(', ')) : ok()
+
+},
+
+noModules = async ()=> {
+  const [ fail, ok ] = test('"npm run unsummon" command supposed to remove all lapland submodules', "and it does!")
+  let modules =
+    'santa, home, api, apielf, seself, pasself, dataelf'.split(', ')
+
+  await exec("npm run unsummon")
+  const left = []
+  for (const m of modules) {
+    try { if ((await fsp.stat(m)).isDirectory()) left.push(m) } catch {}
+  }
+  left.length?
+    fail("but it didn't remove these modules: "+left.join(', ')) : ok()
 }
 
-allModules()
+(async ()=>{
+  await allModules()
+  await noModules()
+})()
 
 setTimeout(c, 36e6)
