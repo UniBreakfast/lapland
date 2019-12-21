58b9897e9e0ab8b7f1ce2dab2fbafdecd0a67832
unibreakfast <unibreakfast1@gmail.com>
2019-12-21 10:50:18 +0200

    lapland.js and index.js added

---
+++ index.js
@@ -0,0 +1,9 @@
+
+const lapland = require('./lapland.js')
+
+const PORT = process.env.PORT || 3000
+
+const options = {PORT}
+
+
+lapland.wish(options)
---
+++ lapland.js
@@ -0,0 +1,12 @@
+
+const
+c = console.log,
+
+
+wish = options => {
+  const { PORT } = options
+
+  c("Lapland is going to be created at port "+PORT)
+}
+
+module.exports = {wish}
